e2f9732ac10bce7545e5cbe081bf800391d8fbbb
İlker Onur Kaya <ilkeronurkaya@gmail.com>
2026-04-13 21:10:01 +0300

    chore(deploy): Remove unintentionally tracked .open-next build cache from git to fix ESBuild bug

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_API_URL":"https://yararlan-api.ilkeronurkaya.workers.dev","NEXT_PUBLIC_GA_ID":"G-E1LGKVSVSM"};
-export const development = {"NEXT_PUBLIC_API_URL":"https://yararlan-api.ilkeronurkaya.workers.dev"};
-export const test = {};
